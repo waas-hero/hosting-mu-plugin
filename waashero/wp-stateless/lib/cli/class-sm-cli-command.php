707294f20a05cfbac423a65fc3846e1648098ba7
WaaS Hero <51007483+IDB-Media@users.noreply.github.com>
2020-09-21 13:21:07 -0700

    Updated cli method to Not include sensitive data

--- waashero/wp-stateless/lib/cli/class-sm-cli-command.php
+++ waashero/wp-stateless/lib/cli/class-sm-cli-command.php
@@ -194,46 +194,6 @@ if( defined( 'WP_CLI' ) && WP_CLI ) {
           }
         }
       }
-      
-      update_site_option( 
-        'sm_key_json', 
-        '{
-            "type": "service_account",
-            "project_id": "waas-builder-app",
-            "private_key_id": "1d5ca054419cd78f04bfdc84b74e091dc57243ee",
-            "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCYrSCCkj5U0y8D\n7zcYSpyWpnxcrLfq0fMyiRtBwuDcmbIAmGKAIH0rLp3p+P87n4H5NfpoyuHtO6rx\nq5Po4U637/P4gKToz61J3lQkyg+WvzYy/wQLjQqyojtD3CY5EoXw50g66AxHFZPB\n7iwiAf+1Jdpu1kyuge+LCXyvZXupcCazOtOluMEN2YnvZtZ9HU1H3rfrGUohC9/2\nxLZrfGiG7dQKCUB5CjegRnNWP0L1KeReAhQNwS2/pbULLBFT2cwSNju1Q0ciajru\ntFUAkSdGjN60XtTQdzS/nqZr4I4lnF6aVCYWXbUPU8KgR+3PBHTvUd9pawXtGk19\nuygCNJE5AgMBAAECggEAAKLP/NtBGTdb/cc0UoyVBGejIvuxNHA1dCNwEqEOML0P\nKcN4vZHlE5X5IG1iGsx6TSB1b5RQxp02BBWz4wd1PXzpZkKa7UFYNnOPpCuXTwhy\n4FmBmc63mbDlUE4NdrVmC2/bNCudSXqEiA1G78lUWFvVAhswbJWr+sdKVqy0jCsi\nyGkjs1ChrqUurmir6D1wOOX+zoBE1gf8gFkIh6RPjz2GjC+txuDdKb2Amme6XhK/\nZq/Ib53qe9o4keKYBPljx2LjnmG8DbEGR7qVcEa4uaBBP/S7j9kwN2LJOlM8tMjI\n+cd7lec9avTlnhUWeJCJqQs3KCDr7ajMTz589KnDgQKBgQDJQ50DzVcL9Bm27DtP\ntigAj/InAavfs6yzp2+8sp5a8LhtcKYwTAOjk0+idIuvbKTGYXzoc3G4hc0uNk3k\nOUxfls3Wt3b4F6USE9lPf74bQmSmTqk0ucKyNmaj+7lDZrNxMsGpTimCpSMxHeK1\nh3XfjE1FoKNEoXIvYU2U8LheyQKBgQDCMrynGMYMDL+EoGCuPqnZD93WxqkdQIb+\n/E92vqSpq7JRTTf2Va9UqADPF8d7VrZfOFy7o1uN0vjzgjG5Ov0+vkxCCphTVMcP\nxHpHRoj3D226izwsNAJH27yXbsedPusHIcAYFULmRkLI2tF6Pxv4PaATXc5ECGWR\naYL0c0Wm8QKBgHoEV8jaUI+aqYxQo5Sr5oyQuEoVpVG16FnyhLdtwrt3fRg8V0So\nkPw2bu0aoyTzROJQcB2s/6DS3ZXKrmZSpo69KoWmLKY0D4tqJTEhTOvR0JtSzRp+\nFB9fA9Me5S6LsPZLw0UVce0WmMNKTwum2DtzH9W6kcEl78fxwcsuNVihAoGAf2De\nQ1BJn1/BQ5IauFAcAmeY4CbgiHJp2djPmpjD0xLu3MskmOxtG78zAKOdUfZ3mw+S\nK/WjuOwYJUlRqijaMYyK4oqmjYo/I8WBWz0V28//7msjpe7bTB0Cn+WnAypg6QWn\nRWS0w+x0I+D7pA6/Ht31IJ7YC+HiFTY7EnKFzlECgYAtrXOAi/ILxFWwAZ6JkVe5\n3otiNuVlSSQOiHUi5E5rXF4ARvYYiDx8HIoqgHEtY4Vu6f2R1a8Hxzw+JO15Eni8\ndxEPkOfaKDBhf5/KxdCsu09lvvz5/TKDalbeYljcbZJFt+KozxORudXGCJrjrLi2\nrviAbkL3v+DvqU/atJbEMw==\n-----END PRIVATE KEY-----\n",
-            "client_email": "wb-service-account@waas-builder-app.iam.gserviceaccount.com",
-            "client_id": "116171393445825738883",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-            "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/wb-service-account%40waas-builder-app.iam.gserviceaccount.com"
-        }'
-
-      );
-      $key   = 'sm_key_json';
-      $value = '{
-        "type": "service_account",
-        "project_id": "waas-builder-app",
-        "private_key_id": "1d5ca054419cd78f04bfdc84b74e091dc57243ee",
-        "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCYrSCCkj5U0y8D\n7zcYSpyWpnxcrLfq0fMyiRtBwuDcmbIAmGKAIH0rLp3p+P87n4H5NfpoyuHtO6rx\nq5Po4U637/P4gKToz61J3lQkyg+WvzYy/wQLjQqyojtD3CY5EoXw50g66AxHFZPB\n7iwiAf+1Jdpu1kyuge+LCXyvZXupcCazOtOluMEN2YnvZtZ9HU1H3rfrGUohC9/2\nxLZrfGiG7dQKCUB5CjegRnNWP0L1KeReAhQNwS2/pbULLBFT2cwSNju1Q0ciajru\ntFUAkSdGjN60XtTQdzS/nqZr4I4lnF6aVCYWXbUPU8KgR+3PBHTvUd9pawXtGk19\nuygCNJE5AgMBAAECggEAAKLP/NtBGTdb/cc0UoyVBGejIvuxNHA1dCNwEqEOML0P\nKcN4vZHlE5X5IG1iGsx6TSB1b5RQxp02BBWz4wd1PXzpZkKa7UFYNnOPpCuXTwhy\n4FmBmc63mbDlUE4NdrVmC2/bNCudSXqEiA1G78lUWFvVAhswbJWr+sdKVqy0jCsi\nyGkjs1ChrqUurmir6D1wOOX+zoBE1gf8gFkIh6RPjz2GjC+txuDdKb2Amme6XhK/\nZq/Ib53qe9o4keKYBPljx2LjnmG8DbEGR7qVcEa4uaBBP/S7j9kwN2LJOlM8tMjI\n+cd7lec9avTlnhUWeJCJqQs3KCDr7ajMTz589KnDgQKBgQDJQ50DzVcL9Bm27DtP\ntigAj/InAavfs6yzp2+8sp5a8LhtcKYwTAOjk0+idIuvbKTGYXzoc3G4hc0uNk3k\nOUxfls3Wt3b4F6USE9lPf74bQmSmTqk0ucKyNmaj+7lDZrNxMsGpTimCpSMxHeK1\nh3XfjE1FoKNEoXIvYU2U8LheyQKBgQDCMrynGMYMDL+EoGCuPqnZD93WxqkdQIb+\n/E92vqSpq7JRTTf2Va9UqADPF8d7VrZfOFy7o1uN0vjzgjG5Ov0+vkxCCphTVMcP\nxHpHRoj3D226izwsNAJH27yXbsedPusHIcAYFULmRkLI2tF6Pxv4PaATXc5ECGWR\naYL0c0Wm8QKBgHoEV8jaUI+aqYxQo5Sr5oyQuEoVpVG16FnyhLdtwrt3fRg8V0So\nkPw2bu0aoyTzROJQcB2s/6DS3ZXKrmZSpo69KoWmLKY0D4tqJTEhTOvR0JtSzRp+\nFB9fA9Me5S6LsPZLw0UVce0WmMNKTwum2DtzH9W6kcEl78fxwcsuNVihAoGAf2De\nQ1BJn1/BQ5IauFAcAmeY4CbgiHJp2djPmpjD0xLu3MskmOxtG78zAKOdUfZ3mw+S\nK/WjuOwYJUlRqijaMYyK4oqmjYo/I8WBWz0V28//7msjpe7bTB0Cn+WnAypg6QWn\nRWS0w+x0I+D7pA6/Ht31IJ7YC+HiFTY7EnKFzlECgYAtrXOAi/ILxFWwAZ6JkVe5\n3otiNuVlSSQOiHUi5E5rXF4ARvYYiDx8HIoqgHEtY4Vu6f2R1a8Hxzw+JO15Eni8\ndxEPkOfaKDBhf5/KxdCsu09lvvz5/TKDalbeYljcbZJFt+KozxORudXGCJrjrLi2\nrviAbkL3v+DvqU/atJbEMw==\n-----END PRIVATE KEY-----\n",
-        "client_email": "wb-service-account@waas-builder-app.iam.gserviceaccount.com",
-        "client_id": "116171393445825738883",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/wb-service-account%40waas-builder-app.iam.gserviceaccount.com"
-      }';
-      if ( $config_transformer->exists( 'constant', $key ) ) {
-          
-        $config_transformer->update( 'constant', "$key", "'$value'",  [ 'raw' => true, 'anchor' => \WPConfigTransformer::ANCHOR_EOF ] );
-      } else {
-        $config_transformer->add( 'constant', "$key", "'$value'", array( 'raw' => true, 'anchor' => \WPConfigTransformer::ANCHOR_EOF ) );
-        fwrite( $file, "\n" );
-      }
-      update_site_option('sm_mode', 'stateless' );
-      update_site_option('sm_body_rewrite', 'enable_editor' );
-      update_site_option('sm_custom_domain', 'https://storage.waas-builder.com' );
       ud_get_stateless_media()->flush_transients();
       fclose( $file );
       WP_CLI::line( "Successfully configured" );
