a2eb9e19c80f4cbafbbbe53a1507ae168e2d4d19
faizanhaidar <faizanhaider594@gmail.com>
2020-11-30 16:03:23 +0500

    shifted processing to post hook and updated paths

--- posthook.sh
+++ posthook.sh
@@ -2,5 +2,26 @@
 #!/bin/bash
 # Create TXT record
 # Test configuration and reload if successful
-sleep 10
-sudo nginx -t && sudo service nginx reload
+sleep 5
+conf="/etc/nginx/conf.d/$CERTBOT_DOMAIN.conf"
+
+cat << EOF > $conf
+server {
+    listen             443 ssl;
+    server_name        $CERTBOT_DOMAIN *.$CERTBOT_DOMAIN;
+    charset            utf-8;
+    ssl_certificate /etc/letsencrypt/live/$CERTBOT_DOMAIN/fullchain.pem; # managed by Certbot
+    ssl_certificate_key /etc/letsencrypt/live/$CERTBOT_DOMAIN/privkey.pem; # managed by Certbot
+    ssl_session_cache          shared:SSL:1m;
+    ssl_session_timeout        10m;
+ 
+    include wpcommon.conf;
+    root /usr/share/wordpress/;
+ 
+    location / {
+        index index.php;
+        try_files \$uri \$uri/ /index.php?\$args;
+        include rh-php-fpm.conf;
+    }
+}
+EOF
