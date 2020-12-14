1ee936811ed4cac6e5ba0858bf62e38b60b80583
faizanhaidar <faizanhaider594@gmail.com>
2020-12-14 23:40:54 +0500

    added cert deletion method

--- posthook.sh
+++ posthook.sh
@@ -24,3 +24,20 @@ server {
     }
 }
 EOF
+
+#!/bin/bash
+# Create TXT record
+full_path=$(realpath $0)
+
+mu_plugin_path=$(dirname $full_path)
+#relative_path=$(dirname $dir_path )
+
+share_folder=$(dirname $mu_plugin_path )
+
+
+API_KEY=`cat "$share_folder/wordpress/wp-config.php" | grep WAASHERO_CLIENT_API_KEY | cut -d \' -f 4`
+
+RECORD_ID=$(curl -s -X DELETE "https://waas-builder.com/api/v0/ultimo/zone/record" \
+     -H     "Authorization: Bearer $API_KEY" \
+     -H     "Content-Type: application/json" \
+     --data '{"domain":"'"$CERTBOT_DOMAIN"'","content":"'"$CERTBOT_VALIDATION"'"}'  )
